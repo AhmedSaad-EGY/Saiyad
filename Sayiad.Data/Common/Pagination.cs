a000071aaa4581a545428474d260fd9f6cd5ec55
Ahmed Mohammed Saad Ahmed <ahmedsaad20169799@gmail.com>
2026-05-13 14:49:06 +0300

    Data Access layer (DAL)

---
+++ Sayiad.Data/Common/Pagination.cs
@@ -0,0 +1,16 @@
+namespace Sayiad.Data.Common;
+
+public class PaginationRequest
+{
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
+}
+
+public class PagedResult<T>
+{
+    public List<T> Items { get; set; } = [];
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+}
