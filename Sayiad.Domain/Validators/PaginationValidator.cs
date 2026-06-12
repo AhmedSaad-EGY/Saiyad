4169998e3f09de3798bf29d50f2a9a4ba707f4d3
AhmedSaad-EGY <ahmedsaad20169799@gmail.com>
2026-06-12 03:51:18 +0300

    fix: increase PaginationValidator PageSize max from 100 to 200
    Frontend sends pageSize=200 on wishlist; FluentValidation was rejecting
    it as outside the 1-100 range, causing a 400 Bad Request.

--- Sayiad.Domain/Validators/PaginationValidator.cs
+++ Sayiad.Domain/Validators/PaginationValidator.cs
@@ -8,6 +8,6 @@ public class PaginationValidator : AbstractValidator<PaginationRequest>
     public PaginationValidator()
     {
         RuleFor(x => x.Page).GreaterThanOrEqualTo(1).WithMessage("Page must be 1 or greater.");
-        RuleFor(x => x.PageSize).InclusiveBetween(1, 100).WithMessage("Page size must be between 1 and 100.");
+        RuleFor(x => x.PageSize).InclusiveBetween(1, 200).WithMessage("Page size must be between 1 and 200.");
     }
 }
